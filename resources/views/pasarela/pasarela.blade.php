40165f616712351024af7e52e087407a9abf7f7d
Juanyonda13 <juan.yonda@misena.edu.co>
2022-12-07 01:28:52 -0500

    vista de la pasarela de pago

--- resources/views/pasarela/pasarela.blade.php
+++ resources/views/pasarela/pasarela.blade.php
@@ -2,11 +2,13 @@
 @section('content')
     <div class="container-fluid">
         <div class="row justify-content-center">
-            <div class="col-11 col-sm-10 col-md-10 col-lg-6 col-xl-5 text-center p-0 mt-3 mb-2">
-                <div class="card px-0 pt-4 pb-0 mt-3 mb-3">
-                    <h2 id="heading">Sign Up Your User Account</h2>
-                    <p>Fill all form field to go to next step</p>
-                    <form id="msform">
+            <div class="col-11 col-sm-10 col-md-10 col-lg-6 col-xl-5 text-center p-0 mt-3 mb-2 ">
+                <div class="d-flex flex-column col-12 card p-2 rounded-5">
+                    <h2 id="heading">Where should we sent the order?</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora accusamus dicta totam, ipsam,
+                        accusantium sapiente harum officia quisquam, ullam velit repellat. Exercitationem quia tenetur
+                        officiis ut temporibus alias non odit.</p>
+                    <form id="msform" class="m-2">
                         <!-- progressbar -->
                         <ul id="progressbar">
                             <li class="active" id="account"><strong>Account</strong></li>
@@ -14,23 +16,34 @@
                             <li id="payment"><strong>Image</strong></li>
                             <li id="confirm"><strong>Finish</strong></li>
                         </ul>
- <!-- fieldsets -->
-                        <fieldset>
+                        <!-- fieldsets -->
+                        <fieldset class="">
                             <div class="form-card">
                                 <div class="row">
                                     <div class="col-7">
                                         <h2 class="fs-title">Account Information:</h2>
                                     </div>
-                                    <div class="col-5">
-                                        <h2 class="steps">Step 1 - 4</h2>
+                                </div>
+                                <div class="col-12 d-flex flex-wrap">
+                                    <div class="col-6">
+                                        <label class="fieldlabels">Email: *</label> <input type="email" name="email"
+                                        placeholder="Email Id" />
                                     </div>
-                                </div> <label class="fieldlabels">Email: *</label> <input type="email" name="email"
-                                    placeholder="Email Id" /> <label class="fieldlabels">Username: *</label> <input
-                                    type="text" name="uname" placeholder="UserName" /> <label
-                                    class="fieldlabels">Password: *</label> <input type="password" name="pwd"
-                                    placeholder="Password" /> <label class="fieldlabels">Confirm Password: *</label> <input
-                                    type="password" name="cpwd" placeholder="Confirm Password" />
-                            </div> <input type="button" name="next" class="next action-button" value="Next" />
+                                    <div class="col-6">
+                                        <label class="fieldlabels">Email: *</label> <input type="email" name="email"
+                                        placeholder="Email Id" />
+                                    </div>
+                                    <div class="col-6">
+                                        <label class="fieldlabels">Email: *</label> <input type="email" name="email"
+                                        placeholder="Email Id" />
+                                    </div>
+                                    <div class="col-6">
+                                        <label class="fieldlabels">Email: *</label> <input type="email" name="email"
+                                        placeholder="Email Id" />
+                                    </div>
+                                </div>
+
+                            </div> <input type="button" name="next" class="next action-button btn" value="Next" />
                         </fieldset>
                         <fieldset>
                             <div class="form-card">
@@ -38,15 +51,26 @@
                                     <div class="col-7">
                                         <h2 class="fs-title">Personal Information:</h2>
                                     </div>
-                                    <div class="col-5">
-                                        <h2 class="steps">Step 2 - 4</h2>
+                   
+                                </div> 
+                                <div class="col-12 d-flex flex-wrap">
+                                    <div class="col-6">
+                                        <label class="fieldlabels">Email: *</label> <input type="email" name="email"
+                                        placeholder="Email Id" />
                                     </div>
-                                </div> <label class="fieldlabels">First Name: *</label> <input type="text" name="fname"
-                                    placeholder="First Name" /> <label class="fieldlabels">Last Name: *</label> <input
-                                    type="text" name="lname" placeholder="Last Name" /> <label
-                                    class="fieldlabels">Contact No.: *</label> <input type="text" name="phno"
-                                    placeholder="Contact No." /> <label class="fieldlabels">Alternate Contact No.: *</label>
-                                <input type="text" name="phno_2" placeholder="Alternate Contact No." />
+                                    <div class="col-6">
+                                        <label class="fieldlabels">Email: *</label> <input type="email" name="email"
+                                        placeholder="Email Id" />
+                                    </div>
+                                    <div class="col-6">
+                                        <label class="fieldlabels">Email: *</label> <input type="email" name="email"
+                                        placeholder="Email Id" />
+                                    </div>
+                                    <div class="col-6">
+                                        <label class="fieldlabels">Email: *</label> <input type="email" name="email"
+                                        placeholder="Email Id" />
+                                    </div>
+                                </div>
                             </div> <input type="button" name="next" class="next action-button" value="Next" /> <input
                                 type="button" name="previous" class="previous action-button-previous" value="Previous" />
                         </fieldset>
@@ -54,15 +78,29 @@
                             <div class="form-card">
                                 <div class="row">
                                     <div class="col-7">
-                                        <h2 class="fs-title">Image Upload:</h2>
+                                        <h2 class="fs-title">Personal Information:</h2>
                                     </div>
-                                    <div class="col-5">
-                                        <h2 class="steps">Step 3 - 4</h2>
+                   
+                                </div> 
+                                <div class="col-12 d-flex flex-wrap">
+                                    <div class="col-6">
+                                        <label class="fieldlabels">Email: *</label> <input type="email" name="email"
+                                        placeholder="Email Id" />
                                     </div>
-                                </div> <label class="fieldlabels">Upload Your Photo:</label> <input type="file"
-                                    name="pic" accept="image/*"> <label class="fieldlabels">Upload Signature
-                                    Photo:</label> <input type="file" name="pic" accept="image/*">
-                            </div> <input type="button" name="next" class="next action-button" value="Submit" /> <input
+                                    <div class="col-6">
+                                        <label class="fieldlabels">Email: *</label> <input type="email" name="email"
+                                        placeholder="Email Id" />
+                                    </div>
+                                    <div class="col-6">
+                                        <label class="fieldlabels">Email: *</label> <input type="email" name="email"
+                                        placeholder="Email Id" />
+                                    </div>
+                                    <div class="col-6">
+                                        <label class="fieldlabels">Email: *</label> <input type="email" name="email"
+                                        placeholder="Email Id" />
+                                    </div>
+                                </div>
+                            </div> <input type="button" name="next" class="next action-button" value="Next" /> <input
                                 type="button" name="previous" class="previous action-button-previous" value="Previous" />
                         </fieldset>
                         <fieldset>
